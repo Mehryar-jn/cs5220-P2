c270e23052a4e213480c400a9651ea70f2ae0340
Mehryar-jn <mehryar.jannesari@gmail.com>
2024-10-24 21:31:09 -0400

    Add files via upload

--- io.hpp
+++ io.hpp
@@ -5,5 +5,6 @@
 
 void write_header(FILE* fp, int n, int framecount, float h);
 void write_frame_data(FILE* fp, int n, sim_state_t* state, int* c);
+void write_frame_ke(FILE* fp, int n, int framecount, sim_state_t* state, int* c);
 
 #endif /* IO_HPP */
--- io_txt.cpp
+++ io_txt.cpp
@@ -18,5 +18,13 @@ void write_frame_data(FILE* fp, int n, sim_state_t* s, int* c)
     for (int i = 0; i < n; ++i, ++p)
         fprintf(fp, "%e %e %e\n", p->x[0], p->x[1], p->x[2]);
 }
+void write_frame_ke(FILE* fp, int n, int framecount, sim_state_t* s, int* c)
+{
+    particle_t* p = s->part;
+    double ke_sum = 0.0;
+    for (int i = 0; i < n; ++i, ++p)
+        ke_sum += ((p->v[0])*(p->v[0]))+((p->v[1])*(p->v[1]))+((p->v[2])*(p->v[2]));
+    fprintf(fp, "%d %e\n", framecount, ke_sum);
+}
 
 #endif /* IO_OUTBIN */
